1e32d95320f56200c9add2084457f0f5b438fcee
Dark Samurai <keshav223.keshav@gmail.com>
2026-04-16 19:34:45 +0530

    chore [todo]: Initialize the Task Manager Spring Boot App.

---
+++ java_training/session4/TaskManager/src/test/java/com/training/todo/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
