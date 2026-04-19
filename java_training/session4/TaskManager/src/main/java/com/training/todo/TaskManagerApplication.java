1deafdab298274bf5bedd031f383c655e67ea11b
Dark Samurai <keshav223.keshav@gmail.com>
2026-04-19 13:40:29 +0530

    [todo]: Modify TaskManagerApplication.

--- java_training/session4/TaskManager/src/main/java/com/training/todo/TaskManagerApplication.java
+++ java_training/session4/TaskManager/src/main/java/com/training/todo/TaskManagerApplication.java
@@ -8,6 +8,14 @@ public class TaskManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
+        System.out.println("\n==========================================");
+        System.out.println("        TODO Application Started!           ");
+        System.out.println("============================================");
+        System.out.println("   API URL  : http://localhost:8080/tasks");
+        System.out.println("   H2 Console: http://localhost:8080/h2-console");
+        System.out.println("   DB URL   : jdbc:h2:mem:tododb");
+        System.out.println("   Username : su");
+        System.out.println("   Password : ''");
+        System.out.println("==========================================\n");
     }
-
 }
