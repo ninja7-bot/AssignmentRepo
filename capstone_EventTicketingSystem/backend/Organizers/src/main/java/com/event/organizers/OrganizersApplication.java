e01c7f7b32741aacdcfd168629e4cf9d3025bf6c
Dark Samurai <keshav223.keshav@gmail.com>
2026-04-20 14:41:02 +0530

    capstone: Initialize Users and Organizers Spring Project.

---
+++ capstone_EventTicketingSystem/backend/Organizers/src/main/java/com/event/organizers/OrganizersApplication.java
@@ -0,0 +1,13 @@
+package com.event.organizers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizersApplication.class, args);
+    }
+
+}
