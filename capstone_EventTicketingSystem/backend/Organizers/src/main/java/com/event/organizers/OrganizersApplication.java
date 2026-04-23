aa0cef0621139dfdcc4457579b01badd61dd510e
Dark Samurai <keshav223.keshav@gmail.com>
2026-04-23 14:02:31 +0530

    config [capstone]: Configure JWT and Database in Organizers.

--- capstone_EventTicketingSystem/backend/Organizers/src/main/java/com/event/organizers/OrganizersApplication.java
+++ capstone_EventTicketingSystem/backend/Organizers/src/main/java/com/event/organizers/OrganizersApplication.java
@@ -8,6 +8,9 @@ public class OrganizersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrganizersApplication.class, args);
+        System.out.println("----------------------------------------------------------");
+        System.out.println("|--> Organizer Service is running on http://localhost:8082");
+        System.out.println("----------------------------------------------------------");
     }
 
 }
