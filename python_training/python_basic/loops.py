465b02c026dba930daa0e48ea23131662335136b
Dark Samurai <keshav223.keshav@gmail.com>
2026-06-11 09:47:43 +0530

    python [basics]: Loops: For and While Loops

---
+++ python_training/python_basic/loops.py
@@ -0,0 +1,60 @@
+"""
+For Loop upto 100
+"""
+
+for i in range(100):
+    print(i)
+print()
+
+
+"""
+Multiplication Table of 5
+"""
+for i in range(1, 11):
+    print(f"5 * {i} = {5 * i}")
+print()
+
+
+"""
+Factorial of a Number
+"""
+num = 5
+fact = 1
+
+while num > 0:
+    fact *= num
+    num -= 1
+
+print(f"Factorial: {fact}")
+print()
+
+
+"""
+Reverse a number
+"""
+num = 4567
+revNum = 0
+
+while num > 0:
+    revNum = revNum * 10 + num % 10
+    num //= 10
+
+print(f"Reverse: {revNum}")
+print()
+
+
+"""
+Check if a number is prime
+"""
+num = 17
+isPrime = True
+
+for i in range(2, int(num ** 0.5) + 1):
+    if num % i == 0:
+        isPrime = False
+        break
+
+if isPrime:
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
