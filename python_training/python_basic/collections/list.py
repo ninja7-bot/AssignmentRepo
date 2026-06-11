d5f117278ddf826366fd2afec8af08dcb44611fe
Dark Samurai <keshav223.keshav@gmail.com>
2026-06-11 11:17:17 +0530

    python [lists]: Operations on a list, reversing a list and odd/even count.

---
+++ python_training/python_basic/collections/list.py
@@ -0,0 +1,58 @@
+lst = [1, 2, 7, 40, 53, 16, 7, 81, 19, 10]
+print(f"Original list: {lst}")
+
+"""
+Sum, Max, Sort, and Remove Duplicates from a list.
+"""
+# Sum of a list
+temp = 0
+for i in lst:
+    temp += i
+print(f"Sum: {temp}")
+
+# Maximum number in a list
+maxNum = lst[0]
+for num in lst:
+    if num > maxNum:
+        maxNum = num
+print(f"Max: {maxNum}")
+
+# Bubble Sort
+for i in range(len(lst)):
+    for j in range(i + 1, len(lst)):
+        if lst[i] > lst[j]:
+            lst[i], lst[j] = lst[j], lst[i]
+print(f"Sorted: {lst}")
+
+# Remove duplicates
+uniqueLst = []
+for x in lst:
+    if x not in uniqueLst:
+        uniqueLst.append(x)
+print(f"Without duplicates: {uniqueLst}")
+print()
+
+
+"""
+Count even and odd numbers in a list.
+"""
+evenCount = oddCount = 0
+
+for num in lst:
+    if num % 2 == 0:
+        evenCount += 1
+    else:
+        oddCount += 1
+
+print(f"Even numbers: {evenCount}")
+print(f"Odd numbers: {oddCount}")
+print()
+
+
+"""
+Reverse a list without using reverse().
+"""
+reversed_lst = []
+for i in range(len(lst) - 1, -1, -1):
+    reversed_lst.append(lst[i])
+print(f"Reversed list: {reversed_lst}")
