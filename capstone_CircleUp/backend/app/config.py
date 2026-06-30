b69a00210689feca0cccbeeacd600b1f82d4a42c
Dark Samurai <keshav223.keshav@gmail.com>
2026-06-30 11:44:05 +0530

    chore [circleup]: Initial project setup and configuration.

---
+++ capstone_CircleUp/backend/app/config.py
@@ -0,0 +1,21 @@
+"""
+Config file fetches the environment variables set in .env file.
+"""
+
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    database_url: str = ""
+    secret_key: str = ""
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
+
+    class Config:
+        env_file = ".env"
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
+settings = get_settings()
